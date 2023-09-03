2cabc124a022871e7167751e9d518cf1ebf9c059
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-03 09:31:32 +0800

    Set up daisy UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,31 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#5779E0',
+
+          secondary: '#d926a9',
+
+          accent: '#1fb2a6',
+
+          neutral: '#2a323c',
+
+          'base-100': '#2A323C',
+
+          info: '#3abff8',
+
+          success: '#36d399',
+
+          warning: '#fbbd23',
+
+          error: '#f87272',
+        },
+      },
+    ],
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
