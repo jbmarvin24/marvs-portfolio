427fd9b1b19adab666abbef6e8d78c616a0a990b
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-10 12:57:26 +0800

    Merged all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-// import About from './components/About';
-// import Hero from './components/Hero';
-// import Skills from './components/Skills';
-// import Portfolio from './components/Portfolio';
-// import ContactMe from './components/ContactMe';
-// import Footer from './components/Footer';
-
 import NavBar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Portfolio from './components/Portfolio';
+import ContactMe from './components/ContactMe';
+import Footer from './components/Footer';
 
 function App() {
-  return <NavBar />;
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Skills />
+      <Portfolio />
+      <ContactMe />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
