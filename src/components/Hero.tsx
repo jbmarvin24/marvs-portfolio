7a2f537ac42a7fabc9ec6d7f3f54910c1bbda5c1
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-13 20:22:22 +0800

    Added contacts on hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent } from 'react';
 import { BiSend } from 'react-icons/bi';
 import { CgMouse } from 'react-icons/cg';
 import { FiArrowDown } from 'react-icons/fi';
+import { FiGithub } from 'react-icons/fi';
+import { RiLinkedinLine } from 'react-icons/ri';
 
 interface HeroProps {}
 
@@ -10,7 +12,17 @@ const Hero: FunctionComponent<HeroProps> = () => {
     <section className="md:py-32 pt-20 pb-14 px-10 max-w-4xl mx-auto">
       <div className="grid md:grid-cols-2 grid-cols-1 gap-12">
         <div className="md:ml-auto">
-          <div className="w-64 h-60 bg-red-400 rounded-md"></div>
+          <div className="flex items-center md:gap-24 gap-16">
+            <div className="flex flex-col gap-6">
+              <a href="https://linkedin.com/in/jbmarvin" target="_blank">
+                <RiLinkedinLine className="w-6 h-6 text-primary" />
+              </a>
+              <a href="https://github.com/jbmarvin24" target="_blank">
+                <FiGithub className="w-6 h-6 text-primary" />
+              </a>
+            </div>
+            <div className="w-64 h-60 bg-red-400 rounded-md"></div>
+          </div>
         </div>
         <div className="text-left">
           <h1 className="text-5xl font-semibold my-3 text-white">
@@ -23,10 +35,13 @@ const Hero: FunctionComponent<HeroProps> = () => {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi,
             molestias.
           </p>
-          <button className="btn btn-primary my-6 capitalize text-white">
+          <a
+            href="#contact"
+            className="my-6 btn btn-primary capitalize text-white"
+          >
             Contact Me
             <BiSend className="w-5 h-5" />
-          </button>
+          </a>
         </div>
       </div>
       <div className="mt-24 md:flex flex-row gap-2 items-center ml-28 hidden text-white">
