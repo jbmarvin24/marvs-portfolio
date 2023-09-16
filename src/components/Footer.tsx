40588de5bf6b917218b789f6b4a2313dc328a6d5
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-16 16:24:51 +0800

    Footer light theme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,21 @@
 import { FunctionComponent } from 'react';
 import { BiUpArrowAlt } from 'react-icons/bi';
 import Title from './common/Title';
+import useThemeStore from '../themeStore';
+import classNames from 'classnames';
 
 interface FooterProps {}
 
 const Footer: FunctionComponent<FooterProps> = () => {
+  const theme = useThemeStore((s) => s.theme);
+
   return (
-    <footer className="mx-auto p-10 bg-[#0E111B] text-white">
+    <footer
+      className={classNames('mx-auto p-10 text-gray-100', {
+        'bg-[#0E111B]': theme === 'dark',
+        'bg-primary': theme === 'light',
+      })}
+    >
       <div className="grid md:grid-cols-2 grid-cols-1 md:px-40 max-w-5xl mx-auto md:justify-items-stretch gap-20">
         <div>
           <Title>Marvin</Title>
@@ -29,16 +38,26 @@ const Footer: FunctionComponent<FooterProps> = () => {
       <div className="text-right">
         <a
           href="#"
-          className="btn btn-primary md:px-1 px-2 md:opacity-70 opacity-80 min-h-0 md:h-8 rounded-md"
+          className={classNames(
+            'btn btn-primary md:px-1 px-2 md:opacity-70 opacity-80 min-h-0 md:h-8 rounded-md',
+            {
+              'border-gray-300': theme === 'light',
+            }
+          )}
         >
           <BiUpArrowAlt className="md:w-5 md:h-5 w-8 h-8" />
         </a>
       </div>
-      <p className="text-center text-xs text-gray-400 md:mt-16 mt-14">
+      <p
+        className={classNames('text-center text-xs md:mt-16 mt-14', {
+          'text-gray-400': theme === 'dark',
+          'text-gray-300': theme == 'light',
+        })}
+      >
         &copy; John Marvin Doniña. All rights reserved |{' '}
         <a
           href="https://www.behance.net/213c339b"
-          className="text-xs text-gray-400 mt-5"
+          className="text-xs mt-5"
           target="_blank"
         >
           Designed by Мария Ларичева
