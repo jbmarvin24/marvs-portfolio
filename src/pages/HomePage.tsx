505d59abfbab0980800082e6cbbbb6807c83ed72
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-19 21:28:53 +0800

    Removed layout component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,16 +3,31 @@ import About from '../components/About';
 import Skills from '../components/Skills';
 import Portfolio from '../components/Portfolio';
 import ContactMe from '../components/ContactMe';
+import classNames from 'classnames';
+import useThemeStore from '../themeStore';
+import NavBar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 function HomePage() {
+  const theme = useThemeStore((s) => s.theme);
+
   return (
-    <>
-      <Hero />
-      <About />
-      <Skills />
-      <Portfolio />
-      <ContactMe />
-    </>
+    <div
+      className={classNames({
+        'text-gray-100': theme === 'dark',
+        'text-gray-900': theme === 'light',
+      })}
+    >
+      <NavBar />
+      <main className="flex flex-col gap-32 lg:my-20 my-16">
+        <Hero />
+        <About />
+        <Skills />
+        <Portfolio />
+        <ContactMe />
+      </main>
+      <Footer />
+    </div>
   );
 }
 
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Layout from './pages/Layout';
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/PortfolioPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Layout />,
     children: [
       {
         index: true,
