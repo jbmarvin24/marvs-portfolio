b4e5041c0528a4b81eff816156dcba7b3da57a25
John Marvin Doniña <jbmarvin24@gmail.com>
2023-09-20 19:52:18 +0800

    Implement Error Page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,33 @@
+import { FunctionComponent } from 'react';
+import { Link, isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import Container from '../components/common/Container';
+
+interface ErrorPage {}
+
+const ErrorPage: FunctionComponent<ErrorPage> = () => {
+  const error = useRouteError();
+
+  console.log(error);
+
+  return (
+    <Container className="my-20 text-center">
+      <div>
+        <h1 className="text-5xl font-bold">Opps!</h1>
+        <p className="mt-5">
+          {isRouteErrorResponse(error)
+            ? `${error.status} ${error.statusText}`
+            : 'An unexpected error occured.'}
+        </p>
+        {isRouteErrorResponse(error) && error.status === 404 && (
+          <div className="mt-5">
+            <Link to="/" className="link">
+              Go to home
+            </Link>
+          </div>
+        )}
+      </div>
+    </Container>
+  );
+};
+
+export default ErrorPage;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import PortfolioPage from './pages/PortfolioPage';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
